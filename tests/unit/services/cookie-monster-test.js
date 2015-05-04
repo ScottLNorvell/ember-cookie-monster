f0e1bd2f6dcc1494e14abb54ef0204e09db3ada1
Scott Norvell <scottlnorvell@gmail.com>
2015-05-03 23:56:15 -0400

    Stub _putInOven for tests

--- tests/unit/services/cookie-monster-test.js
+++ tests/unit/services/cookie-monster-test.js
@@ -11,7 +11,7 @@ moduleFor('service:cookie-monster', {
         return 'scott=awesome; fake_user_id=42';
       })
     });
-    return klass.create();
+    return klass.create(attrs);
   }
 });
 
@@ -34,14 +34,24 @@ test('cookies can be eaten', function(assert) {
 });
 
 test('cookies can be baked', function(assert) {
-  var service = this.subject();
+  assert.expect(3);
+  var service = this.subject({
+    _putInOven(cookie) {
+      assert.ok(/queens=js/.test(cookie), 'we set a prop on the ACTUAL cookie');
+    }
+  });
   service.bake('queens', 'js');
   assert.equal(service.eat('queens'), 'js', 'queens IS js');
   assert.equal(service.get('cookies.queens'), 'js', 'queens IS js');
 });
 
 test('cookies can be burned', function(assert) {
-  var service = this.subject();
+  assert.expect(3);
+  var service = this.subject({
+    _putInOven(cookie) {
+      assert.ok(/fake_user_id=;/.test(cookie));
+    }
+  });
   service.burn('fake_user_id');
   assert.equal(service.eat('fake_user_id'), null, 'there IS no fake user...');
   assert.equal(service.get('cookies.fake_user_id'), null, 'there IS no fake user...');
